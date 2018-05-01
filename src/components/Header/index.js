41c2536ca408ef80116bab8daf3cea130d51ae01
claudina sarahe <cs@misscs.me>
2018-05-01 10:59:50 -0400

    Header content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,22 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Header = ({ siteTitle }) => (
+import Nav from '../Nav'
+
+
+const Header = ({ data, siteTitle }) => (
   <header
-    className="bg-light-gray"
+    className="pb5 relative"
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
+    <Nav data={data.dataYaml.nav} />
+    <div className="site-header-background"></div>
+      <h1 className="white fw3 lh-tight f-subheadline z-1 relative w-60 pl5 pv3 mt4 mb0">
+        {siteTitle}
       </h1>
-    </div>
+      <h2 className="white fw3 lh-title f2 z-1 relative pl5 light-yellow mt0">
+        <span className="db">June 11-12, 2018</span>
+        <span className="db">Prime Produce, NYC</span>
+      </h2>
   </header>
 )
 
