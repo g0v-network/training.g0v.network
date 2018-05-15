65baa24193bb35e1c0f4669e28aac60ead5682c2
Darshana Narayanan <DZNarayanan@users.noreply.github.com>
2018-05-15 18:34:11 -0400

    copy edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = ({ data }) => (
     <section id="#about" className="w-60-l f4 f3-l lh-copy ml5-l ph3 pv4-m measure-wide-m pl4-m pl5-l mt6 mt5-m mb5-l">
       <p>In the spirit of international exchange and with the support of the Taiwanese government, we are pleased to announce the first English language opportunity to learn the methods that power the award-winning vTaiwan open consultation process and Participatory Officers program. </p> 
   
-      <p>The training will be highly interactive and led by a delegation from Taiwan, including: Digital Minister Audrey Tang and members of the Public Digital Innovation and Service team. We invite you to be an integral part of this inaugural opportunity!
+      <p>This two day training in NYC will be highly interactive and led by a delegation from Taiwan, including: Digital Minister Audrey Tang and members of the Public Digital Innovation and Service team. We invite you to be an integral part of this inaugural opportunity!
 </p>
     </section>
 
