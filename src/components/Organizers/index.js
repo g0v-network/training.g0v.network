f74fede6abcc255828072e16693c4997edd70c5a
Darshana Narayanan <DZNarayanan@users.noreply.github.com>
2018-06-09 11:13:47 -0400

    added space before 'Join us...' (#35)

--- src/components/Organizers/index.js
+++ src/components/Organizers/index.js
@@ -39,7 +39,7 @@ const Organizers = ({data}) => {
             >
               vNetwork is a a collection of nobodies moved by the work in Taiwan to learn and bring the practices to their cities.
 
-              Our group meets online and offline.Join us today by signing up
+              Our group meets online and offline. Join us today by signing up
               for g0v slack.
             </div>
           </article>
